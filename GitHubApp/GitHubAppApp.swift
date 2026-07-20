b1c6a52bd02d1972dc8529bedfd0d0d578c7936b
João Pedro Volponi <jpvrosada@hotmail.com>
2026-07-19 22:22:37 -0300

    Criação do Projeto

---
+++ GitHubApp/GitHubAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubAppApp.swift
+//  GitHubApp
+//
+//  Created by João Pedro Volponi on 19/07/26.
+//
+
+import SwiftUI
+
+@main
+struct GitHubAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
